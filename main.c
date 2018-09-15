3e377110c0324c1499ae7eefc415efb30edbc2a8
jm789016 <jm789016@ohio.edu>
2018-09-15 00:10:29 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   int x = 2;
+
+   int y = x+3;
+
+   int z = y+x;
+
+
+
+    printf("Value of x = %d\n", x);
+
+    printf("Value of y = %d\n", y);
+
+    printf("Value of z = %d\n", z);
+    return 0;
+}
